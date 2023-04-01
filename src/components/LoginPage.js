555310c5dbb68cd460d2837480f2fd3d25fc1d60
Noraphat <punpunping@gmail.com>
2023-04-01 16:13:56 +0100

    styled login page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,44 +1,49 @@
-import React, { useState } from 'react';
-import LoginPageCSS from '../css/loginpage.module.css';
+import React, { useState } from "react";
+import LoginPageCSS from "../css/loginpage.module.css";
 
 function LoginPage() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log('Email:', email, 'Password:', password);
-    // Add your authentication logic here
-  };
+	const handleSubmit = e => {
+		e.preventDefault();
+		console.log("Email:", email, "Password:", password);
+		// Add your authentication logic here
+	};
 
-  return (
-    <div className={LoginPageCSS.container}>
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
+	return (
+		<div className={LoginPageCSS.container}>
+			<div className={LoginPageCSS["sub-container"]}>
+				<p className={LoginPageCSS.header}>Login</p>
+				<form
+					onSubmit={handleSubmit}
+					className={LoginPageCSS.form}
+				>
+					<div>
+						<label htmlFor="email">Username</label>
+						<input
+							type="email"
+							id="email"
+							value={email}
+							onChange={e => setEmail(e.target.value)}
+							required
+						/>
+					</div>
+					<div>
+						<label htmlFor="password">Password</label>
+						<input
+							type="password"
+							id="password"
+							value={password}
+							onChange={e => setPassword(e.target.value)}
+							required
+						/>
+					</div>
+					<button type="submit" className={LoginPageCSS['login-button']}>Login</button>
+				</form>
+			</div>
+		</div>
+	);
 }
 
 export default LoginPage;
