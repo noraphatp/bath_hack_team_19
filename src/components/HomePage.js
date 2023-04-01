bbc046818c69ef0602e343e136d777f790d011da
Noraphat <punpunping@gmail.com>
2023-04-01 22:09:21 +0100

    commented out import (unused)

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 // import { useHistory } from "react-router-dom";
-import Navbar from "./Navbar";
+// import Navbar from "./Navbar";
 import HomePageCSS from "../css/homepage.module.css";
 import logo from "./logo.png";
 
