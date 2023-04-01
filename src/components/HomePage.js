e5cafc202b4cac918c81ffff861ca62099691d9e
Noraphat <punpunping@gmail.com>
2023-04-01 13:34:03 +0100

    added home page skeleton code

---
+++ src/components/HomePage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import HomePageCSS from '../css/homePage.module.css';
+
+export default function HomePage() {
+  return (
+    <div className={HomePageCSS.container}>
+      
+    </div>
+  );
+}
