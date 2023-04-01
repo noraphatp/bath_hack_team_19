d2f6702a373e4f0ac80aee5f418d27a4495eb00a
Dan Parsley <danparsley03@gmail.com>
2023-04-01 16:18:38 +0100

    Added unstyled navbar with logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,34 @@
 import React from 'react';
-import NavbarCSS from '../css/navbar.module.css';
+import styles from '../css/navbar.module.css';
+import { Link } from 'react-router-dom';
+import logo from './logo.png';
+
 
 export default function Navbar() {
   return (
-    <nav className={NavbarCSS.container}>
-      <div className={NavbarCSS['brand-section']}>
-        <p className={NavbarCSS['brand-name']}>Brand</p>
+    <nav className={styles.navbar}>
+      <div className={styles.navbarContainer}>
+      <Link to="/" className={styles.navbarLogo}>
+        <img src={logo} alt="Logo" />
+      </Link>
+        <ul className={styles.navbarMenu}>
+          <li className={styles.navbarItem}>
+            <Link to="/jobs" className={styles.navbarLink}>
+              Jobs
+            </Link>
+          </li>
+          <li className={styles.navbarItem}>
+            <Link to="/about" className={styles.navbarLink}>
+              About the Project
+            </Link>
+          </li>
+          <li className={styles.navbarItem}>
+            <Link to="/account" className={styles.navbarLink}>
+              My Account
+            </Link>
+          </li>
+        </ul>
       </div>
     </nav>
   );
-}
+};
