11a14e448664baaf6d9c08ac9e01eb5976d7959d
Noraphat <punpunping@gmail.com>
2023-04-01 21:11:31 +0100

    removed about page from routes

--- src/Routes.js
+++ src/Routes.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import LoginPage from "./components/LoginPage";
 import SignupPage from "./components/SignupPage";
-import About from "./components/AboutPage";
 import UserList from "./components/UserList";
 import FindJobs from "./components/FindJobs";
 import Accessibility from "./components/Accessibility"; // Import the Accessibility component
@@ -34,10 +33,6 @@ const AppRoutes = () => {
 					element={<Accessibility />}
 				/>{" "}
 				{/* Add a route for Accessibility */}
-				<Route
-					path="/about"
-					element={<About />}
-				/>{" "}
 				{/* Add a route for About */}
 			</Routes>
 		</Router>
