09bbb4eb81d4ca9bd603f0c8294b9bb0010d344d
pongpradk <111737114+pongpradk@users.noreply.github.com>
2023-04-02 01:17:13 +0100

    added keyboard navigation to Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,10 +6,9 @@ import "../css/styles.css"; // Keep the import statement
 import { auth, onAuthStateChanged } from "../firebase";
 
 export default function Navbar() {
-  
   const [user, setUser] = useState(null);
   const [loggedIn, setLoggedIn] = useState(false);
-
+  const [selectedInput, setSelectedInput] = useState(null);
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
@@ -19,12 +18,32 @@ export default function Navbar() {
         setLoggedIn(false);
       }
     });
-  
+
+    const handleKeyDown = (event) => {
+      if (!selectedInput) {
+        switch (event.key) {
+          case "1":
+            window.location.href = "/find-jobs";
+            break;
+          case "2":
+            window.location.href = "/accessibility";
+            break;
+          case "3":
+            window.location.href = "/login";
+            break;
+          default:
+            break;
+        }
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+
     return () => {
       unsubscribe();
+      document.removeEventListener("keydown", handleKeyDown);
     };
-  }, []);
-   
+  }, [selectedInput]);
 
   return (
     <nav className={styles.navbar}>
@@ -33,31 +52,30 @@ export default function Navbar() {
           <img src={logo} alt="Logo" />
         </Link>
         <ul className={styles.navbarMenu}>
-  <li className={styles.navbarItem}>
-    <Link to="/find-jobs" className={styles.navbarLink}>
-      Find Jobs
-    </Link>
-  </li>
-  <li className={styles.navbarItem}>
-    <Link to="/accessibility" className={styles.navbarLink}>
-      Accessibility
-    </Link>
-  </li>
-  {loggedIn ? (
-    <li className={styles.navbarItem}>
-      <Link to="/account" className={styles.navbarLink}>
-        My Account
-      </Link>
-    </li>
-  ) : (
-    <li className={styles.navbarItem}>
-      <Link to="/login" className={styles.navbarLink}>
-        Login
-      </Link>
-    </li>
-  )}
-</ul>
-
+          <li className={styles.navbarItem}>
+            <Link to="/find-jobs" className={styles.navbarLink}>
+              Find Jobs
+            </Link>
+          </li>
+          <li className={styles.navbarItem}>
+            <Link to="/accessibility" className={styles.navbarLink}>
+              Accessibility
+            </Link>
+          </li>
+          {loggedIn ? (
+            <li className={styles.navbarItem}>
+              <Link to="/account" className={styles.navbarLink}>
+                My Account
+              </Link>
+            </li>
+          ) : (
+            <li className={styles.navbarItem}>
+              <Link to="/login" className={styles.navbarLink}>
+                Login
+              </Link>
+            </li>
+          )}
+        </ul>
       </div>
     </nav>
   );
